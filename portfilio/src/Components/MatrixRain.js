2269f865abf8556e3db6ce060e846ff08b5d1d36
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2022-04-15 15:27:03 +0100

    feat:create Matrix intro fix #1

---
+++ portfilio/src/Components/MatrixRain.js
@@ -0,0 +1,9 @@
+import "./MatrixRain.css";
+import RainStream from "./RainStream";
+
+const MatrixRain = () =>{
+    return <div className="background">
+        <RainStream /> </div>
+};
+
+export default MatrixRain;
