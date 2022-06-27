c4b4de2bbb990fd17cc94086818908efa1fa5a00
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2022-06-27 19:48:58 +0100

    Update MatrixRain.js

--- portfilio/src/Components/MatrixRain.js
+++ portfilio/src/Components/MatrixRain.js
@@ -1,9 +1,13 @@
 import "./MatrixRain.css";
 import RainStream from "./RainStream";
 
-const MatrixRain = () =>{
-    return <div className="background">
-        <RainStream /> </div>
+
+const MatrixRain = () => {
+  return (
+    <div className='background'>
+      <RainStream />
+    </div>
+  );
 };
 
 export default MatrixRain;
