6981e6121557813e9b1ab075999fcaa59e0c9d60
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2023-01-16 13:45:56 +0000

    correct the codes and clean it

--- portfilio/src/PortfolioContainer/Home/Profile.js
+++ portfilio/src/PortfolioContainer/Home/Profile.js
@@ -7,60 +7,65 @@ export default function Profile() {
       <div className='profile-parent'>
         <div className='profile-details'>
           <div className='colz'>
-            <a href='https://www.facebook.com/MaziarMajd13/'>
-              <i className='fa fa-facebook-square'></i>
-            </a>
-            <a href='https://www.linkedin.com/in/maziarmajd/'>
-              <i className='fa fa-linkedin-square'></i>
-            </a>
-            <a href='https://twitter.com/MajdMaziar'>
-              <i className='fa fa-twitter-square'></i>
-            </a>
-            <a href='https://github.com/Maziarmajd'>
-              <i className='fa fa-github-square'></i>
-            </a>
-            <div className='profile-details-name'>
-              <span className='primary-text'>
+            <div className='colz-icon'>
+              <a href='https://www.facebook.com/MaziarMajd13/'>
+                <i className='fa fa-facebook-square'></i>
+              </a>
+              <a href='https://www.linkedin.com/in/maziarmajd/'>
+                <i className='fa fa-linkedin-square'></i>
+              </a>
+              <a href='https://twitter.com/MajdMaziar'>
+                <i className='fa fa-twitter-square'></i>
+              </a>
+              <a href='https://github.com/Maziarmajd'>
+                <i className='fa fa-github-square'></i>
+              </a>
+            </div>
+          </div>
+          <div className='profile-details-name'>
+            <span className='primary-text'>
+              {" "}
+              Hello, I'M <span className='highlighted-text'>Maziar</span>
+            </span>
+          </div>
+          <div className='profile-details-role'>
+            <span className='primary-text'>
+              {" "}
+              <h1>
                 {" "}
-                Hello, I'M <span className='highlighted-text'>Maziar</span>
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Enthusiastic Dev 🌐",
+                    1000,
+                    "FullStack Developer 🧑🏻‍💻",
+                    1000,
+                    "FrontEnd Developer 🖥️",
+                    1000,
+                    "Ui/Ux designer 🎨",
+                    1000,
+                    "React Dev 💻",
+                    1000,
+                  ]}
+                />
+              </h1>
+              <span className='profile-role-tagline'>
+                Knack of building applications with front and back end
+                operations.
               </span>
-            </div>
-            <div className='profile-details-role'>
-              <span className='primary-text'>
-                {" "}
-                <h1>
-                  {" "}
-                  <Typical
-                    loop={Infinity}
-                    steps={[
-                      "Enthusiastic Dev 🌐",
-                      1000,
-                      "FullStack Developer 🧑🏻‍💻",
-                      1000,
-                      "FrontEnd Developer 🖥️",
-                      1000,
-                      "Ui/Ux designer 🎨",
-                      1000,
-                      "React Dev 💻",
-                      1000,
-                    ]}
-                  />
-                </h1>
-                <span className='profile-role-tagline'>
-                  Knack of building applications with front and back end
-                  operations.
-                </span>
-              </span>
-            </div>
-            <div className="profile-optins">
-                <button className="btn primary-btn">
-                    {""}
-                    Hire Me{" "}
-                </button>
-                <a href="MaziarMajdCV.pdf" download="MaziarMajdCV.pdf">
-                <button className="btn highlighted-btn">Get My CV</button>
-                </a>
-            </div>
+            </span>
+          </div>
+          <div className='profile-optins'>
+            <button className='btn primary-btn'>
+              {""}
+              Hire Me{" "}
+            </button>
+            <a href='MaziarMajdCV.pdf' download='MaziarMajdCV.pdf'>
+              <button className='btn highlighted-btn'>Get My CV</button>
+            </a>
+          </div>
+          <div className='profile-picture'>
+            <div className='profile-picture-background'></div>
           </div>
         </div>
       </div>
