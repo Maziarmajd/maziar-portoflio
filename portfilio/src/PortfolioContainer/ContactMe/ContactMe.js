289cac3bb4ad65a29c4bcba4607071530c450fc1
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2023-02-14 13:04:39 +0000

    contact me form

--- portfilio/src/PortfolioContainer/ContactMe/ContactMe.js
+++ portfilio/src/PortfolioContainer/ContactMe/ContactMe.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Typical from "react-typical";
 import imgBack from "../../../src/images/mailz.jpeg";
 import load1 from "../../../src/images/load2.gif";
@@ -14,6 +14,22 @@ export default function ContactMe(props) {
 
   const fadeInSubscription =
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [banner, setBanner] = useState("");
+  const [bool, setBool] = useState(false);
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleMessage = (e) => {
+    setMessage(e.target.value);
+  };
 
   return (
     <div className='main-container' id={props.id || ""}>
@@ -36,6 +52,30 @@ export default function ContactMe(props) {
           <a href='https://github.com/Maziarmajd'>
             <i className='fa fa-github-square'></i>
           </a>
+        </div>
+        <div className='back-form'>
+          <div className='img-back'>
+            <h4>Send Your Email Here</h4>
+            <img src={imgBack} alt='image Not Found' />
+          </div>
+          <form>
+            <p>{banner}</p>
+            <label htmlFor='name'>Name</label>
+            <input type='text' onChange={handleName} value={name} />
+
+            <label htmlFor='email'>Email</label>
+            <input type='email' onChange={handleEmail} value={email} />
+
+            <label htmlFor='message'>Message</label>
+            <textarea type='text' onChange={handleMessage} value={message} />
+
+            <div className='send-btn'>
+              <button type='submit'>
+                Send
+                <i className='fa fa-paper-plane' />
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
