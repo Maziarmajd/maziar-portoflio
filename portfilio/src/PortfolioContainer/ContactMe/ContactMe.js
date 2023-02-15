5bb5caaa90761408e0780e2044ed70dcda4840cb
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2023-02-15 17:00:55 +0000

    backend Initialization

--- portfilio/src/PortfolioContainer/ContactMe/ContactMe.js
+++ portfilio/src/PortfolioContainer/ContactMe/ContactMe.js
@@ -36,7 +36,8 @@ export default function ContactMe(props) {
   const handleMessage = (e) => {
     setMessage(e.target.value);
   };
-
+  console.log(name);
+  
   const submitForm = async (e) => {
     e.preventDefault();
     try {
