2db76f47df06b73a208f978e209ba9f8dcbd5d17
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2023-02-15 18:08:37 +0000

    configure email

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-require("dotenv").config
-const express =require("express")
-const cors =  require("cors")
-const path = require("path")
+require("dotenv").config;
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const contactRoute = require("./route/contactRoute");
 
 const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 const port = process.env.PORT || 5000;
 
